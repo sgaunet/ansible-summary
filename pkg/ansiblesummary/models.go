f015125b24a963c1819dae5edfbb6b4b79c99820
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2024-02-08 21:11:44 +0100

    package: add comments

--- pkg/ansiblesummary/models.go
+++ pkg/ansiblesummary/models.go
@@ -1,5 +1,40 @@
 package ansiblesummary
 
+// AnsibleSummary represents the struct of the json output
+// of the ansible-playbook command if env var below are positionned
+// export ANSIBLE_CALLBACKS_ENABLED=json
+// export ANSIBLE_STDOUT_CALLBACK=json
+type AnsibleSummary struct {
+	Plays []Plays         `json:"plays"`
+	Stats map[string]Stat `json:"stats"` // one stat by hostname
+}
+
+// Plays is the struct to describe a whole run of playbook
+// There is basic playbook information (Play)
+// And the tasks
+type Plays struct {
+	Play  Play    `json:"play"`
+	Tasks []Tasks `json:"tasks"`
+}
+
+// Play represents informations about playbook (omit duration, not needed)
+type Play struct {
+	Name string `json:"name"`
+	Id   string `json:"id"`
+}
+
+// Tasks represents the tasks of playbook
+type Tasks struct {
+	Hosts map[string]Host `json:"hosts"`
+	Task  Task            `json:"task"`
+}
+
+// Task represents informations about task of playbook (omit informations)
+type Task struct {
+	Name string `json:"name"`
+}
+
+// Stats of tasks of the playbook for one host
 type Stat struct {
 	Changed     int `json:"changed"`
 	Failures    int `json:"failures"`
@@ -10,30 +45,6 @@ type Stat struct {
 	Unreachable int `json:"unreachable"`
 }
 
-type Play struct {
-	Name string `json:"name"`
-	Id   string `json:"id"`
-}
-
-type Task struct {
-	Name string `json:"name"`
-}
-
 type Host struct {
 	Changed bool `json:"changed"`
-}
-
-type Tasks struct {
-	Hosts map[string]Host `json:"hosts"`
-	Task  Task            `json:"task"`
-}
-
-type Plays struct {
-	Play  Play    `json:"play"`
-	Tasks []Tasks `json:"tasks"`
-}
-
-type AnsibleSummary struct {
-	Plays []Plays         `json:"plays"`
-	Stats map[string]Stat `json:"stats"`
 }
